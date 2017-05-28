05fe1038a8174df598263b9101723518e7d89be9
Daniel Czerwonk <daniel@dan-nrw.de>
2017-05-28 16:51:25 +0200

    added config file support

---
+++ configuration/config.go
@@ -0,0 +1,57 @@
+package configuration
+
+import (
+	"os"
+
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Unifi  []*UnfiConfig   `yaml:"unifi,omitempty"`
+	Ruckus []*RuckusConfig `yaml:"ruckus,omitempty"`
+}
+
+type UnfiConfig struct {
+	ApiUrl  string `yaml:"api_url"`
+	ApiUser string `yaml:"api_user"`
+	ApiPass string `yaml:"api_password"`
+}
+
+type RuckusConfig struct {
+	Host string `yaml:"host"`
+}
+
+// Load reads config file
+func Load(path string) (*Config, error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return loadFile(f)
+}
+
+func loadFile(f *os.File) (*Config, error) {
+	b, err := ioutil.ReadAll(f)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return unmarshal(b)
+}
+
+func unmarshal(b []byte) (*Config, error) {
+	var c Config
+	err := yaml.Unmarshal(b, &c)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
--- main.go
+++ main.go
@@ -6,21 +6,21 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/czerwonk/wifi_exporter/configuration"
 	"github.com/czerwonk/wifi_exporter/unifi"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.3.0"
+const version string = "0.4.0"
 
 var (
 	showVersion   = flag.Bool("version", false, "Print version information.")
 	listenAddress = flag.String("web.listen-address", ":9120", "Address on which to expose metrics and web interface.")
 	metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
-	apiUrl        = flag.String("api.url", "http://unifi", "base URL to the Unifi Controller API")
-	apiUser       = flag.String("api.user", "username", "username to access the Unifi Controller API")
-	apiPass       = flag.String("api.pass", "password", "password to authorize user")
+	configPath    = flag.String("config.path", "config.yml", "Path to config file")
+	config        *configuration.Config
 )
 
 func main() {
@@ -31,6 +31,12 @@ func main() {
 		os.Exit(0)
 	}
 
+	err := loadConfig()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
 	startServer()
 }
 
@@ -39,6 +45,17 @@ func printVersion() {
 	fmt.Printf("Version: %s\n", version)
 	fmt.Println("Author(s): Daniel Czerwonk")
 	fmt.Println("Metric exporter for wifi controllers")
+}
+
+func loadConfig() error {
+	var err error
+	config, err = configuration.Load(*configPath)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func startServer() {
@@ -60,14 +77,9 @@ func errorHandler(f func(http.ResponseWriter, *http.Request) error) http.Handler
 }
 
 func handleMetricsRequest(w http.ResponseWriter, r *http.Request) error {
-	c, err := unifi.NewUnifiCollector(*apiUrl, *apiUser, *apiPass)
-
-	if err != nil {
-		return err
-	}
-
 	reg := prometheus.NewRegistry()
-	reg.MustRegister(c)
+	addUnifiCollectors(reg)
+	addRuckusCollectors(reg)
 
 	h := promhttp.HandlerFor(reg, promhttp.HandlerOpts{
 		ErrorLog:      log.NewErrorLogger(),
@@ -75,4 +87,20 @@ func handleMetricsRequest(w http.ResponseWriter, r *http.Request) error {
 	h.ServeHTTP(w, r)
 
 	return nil
+}
+
+func addUnifiCollectors(reg *prometheus.Registry) {
+	for _, u := range config.Unifi {
+		c, err := unifi.NewUnifiCollector(u.ApiUrl, u.ApiUser, u.ApiPass)
+
+		if err != nil {
+			log.Errorln(err)
+		} else {
+			reg.MustRegister(c)
+		}
+	}
+}
+
+func addRuckusCollectors(reg *prometheus.Registry) {
+
 }
